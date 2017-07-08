f56ccb92b3d556b9eb80918fae4d52f8e230c268
zzzzzyx <zzzzzyx@outlook.com>
2017-07-08 15:13:43 +0800

    搭建基本eclipse项目框架

---
+++ film_comment/src/main/java/com/zzzzzyx/film_comment/model/App.java
@@ -0,0 +1,13 @@
+package com.zzzzzyx.film_comment.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
